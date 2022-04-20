c9d43fb720b09b8deeb50d0889b19ade5b28787a
Rachel Freeland <rachel.freeland1@gmail.com>
2022-04-20 01:14:00 -0500

    Problem with factorial even after importing math module

---
+++ math_series/series.py
@@ -0,0 +1,12 @@
+import math
+
+def fibonacci(n):
+  if n <= 1:
+    return 1
+  
+
+def lucas():
+  pass
+
+def sum_series():
+  pass
---
+++ tests/test_series.py
@@ -0,0 +1,7 @@
+import math
+from math_series import fibonacci
+
+def test_fibonacci_one():
+  actual = factorial(1)
+  expected = 1
+  assert actual == expected
